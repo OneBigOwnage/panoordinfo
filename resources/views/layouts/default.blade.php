0a959e4e20adee38a69cde15d886888886fb6841
OneBigOwnage <niekvandenbos@hotmail.com>
2019-12-10 18:07:37 +0100

    Adjust the default layout.

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,4 +1,3 @@
-<!doctype html>
 <html lang="en">
 
 <head>
@@ -13,7 +12,6 @@
 
   <!-- Main Style -->
   <link rel="stylesheet" href="{{ mix('/css/admin-app.css') }}" id="main-css">
-  <link rel="stylesheet" href="#" id="sidebar-css">
 
   <title>Panoord infoscherm</title>
 </head>
@@ -27,8 +25,8 @@
     <div class="sidebar-header">
       <!-- Logo -->
       <a href="index.html" class="logo d-flex align-items-center">
-        <img src="../img/logo.svg" alt="Logo" id="main-logo">
-        Panoord infoscherm
+        <img src="/img/logo.svg" alt="Logo" id="main-logo">
+        <span>Panoord infoscherm</span>
       </a>
       <a href="#" class="nav-link nav-icon rounded-circle ml-auto" data-toggle="sidebar">
         <i class="material-icons">close</i>
@@ -65,11 +63,11 @@
       <li class="nav-link-divider mx-2"></li>
       <li class="nav-item dropdown">
         <a class="nav-link has-img dropdown-toggle" href="#" data-toggle="dropdown">
-          <img src="../img/user.svg" alt="Admin" class="rounded-circle mr-2">
-          <span class="d-none d-sm-block">Admin</span>
+          <img src="/img/user.svg" alt="{{ optional(auth()->user())->name ?? 'Gebruiker' }}" class="rounded-circle mr-2">
+          <span class="d-none d-sm-block">{{ optional(auth()->user())->name ?? 'Gebruiker' }}</span>
         </a>
         <div class="dropdown-menu dropdown-menu-right font-size-sm">
-          <a class="dropdown-item has-icon pr-5 text-danger" href="../html/login.html"><i class="material-icons mr-2">exit_to_app</i> Logout</a>
+          <a class="dropdown-item has-icon pr-5 text-danger" href="logout"><i class="material-icons mr-2">exit_to_app</i> Logout</a>
         </div>
       </li>
     </ul>
@@ -87,6 +85,9 @@
 
   <!-- Main Scripts -->
   <script src="{{ mix('/js/admin-app.js') }}"></script>
+  <script>
+    window.csrf_token = '{{ csrf_token() }}';
+  </script>
 
 </body>
 
