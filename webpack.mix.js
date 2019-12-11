77174271a2df55ca1a03f24736fb60b5205a8b7d
OneBigOwnage <niekvandenbos@hotmail.com>
2019-12-11 18:23:45 +0100

    Configure webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,15 @@ const mix = require('laravel-mix');
  |
  */
 
+/**
+ * We want versioning on all of our files, so we don't have to force-reload or clear the browser cache every time we
+ * make a change or deploy a new version.
+ */
+mix.version();
+
+/**
+ * Admin panel assets.
+ */
 mix.scripts([
     // Core JavaScript files:
     'resources/js/mimity-script.js',
@@ -30,4 +39,20 @@ mix.styles([
     'node_modules/flatpickr/dist/flatpickr.css',
 ], 'public/css/admin-app.css');
 
+
+/**
+ * Info screen assets.
+ */
+mix.scripts([
+    'resources/js/screen.js'
+], 'public/js/screen.js');
+
+mix.styles([
+    'resources/css/screen-styles.css'
+], 'public/css/screen-styles.css');
+
+
+/**
+ * Images, which do not have to be compiled and/or bundled.
+ */
 mix.copy('resources/img', 'public/img');
