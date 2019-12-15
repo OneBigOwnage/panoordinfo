6aca04ddeec17a5d2b51317f00322280446682b4
OneBigOwnage <niekvandenbos@hotmail.com>
2019-12-15 17:48:10 +0100

    Add announcements migration.

---
+++ database/migrations/2019_12_12_033917_create_announcements_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnnouncementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('announcements', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->text('title');
+            $table->longText('contents');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('announcements');
+    }
+}
