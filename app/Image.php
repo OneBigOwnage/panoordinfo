0505c37ef0c2caa633ff226376d778efcf7c82ec
OneBigOwnage <niekvandenbos@hotmail.com>
2019-12-22 13:49:38 +0100

    Add image class.

---
+++ app/Image.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * @property string $original_file_name
+ * @property string $file_path
+ * @property string $mime_type
+ * @property int    $size
+ */
+class Image extends Model
+{
+    use SoftDeletes;
+
+    protected $guarded = [ ];
+
+    /**
+     * Retrieve the absolute file path on disk, of the image file linked to this recordl
+     *
+     * @return string The absolute file path of the image file.
+     */
+    public function absoluteFilePath()
+    {
+        return storage_path('app/' . $this->file_path);
+    }
+
+    /**
+     * Turn the image file that is linked to this record into a file response, which will show the image in the
+     * browser.
+     *
+     * @return \Symfony\Component\HttpFoundation\BinaryFileResponse The generated response.
+     */
+    public function toShowResponse()
+    {
+        return response()->file(
+            $this->absoluteFilePath()
+        );
+    }
+
+    /**
+     * Delete the image file that is linked to this record.
+     *
+     * @return Image $this.
+     */
+    public function unlinkFile()
+    {
+        unlink($this->absoluteFilePath());
+
+        return $this;
+    }
+}
