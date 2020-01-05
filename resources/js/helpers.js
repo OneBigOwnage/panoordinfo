445a4ba2832df2303307ad660b0ddadeb4e16ba1
OneBigOwnage <niekvandenbos@hotmail.com>
2020-01-05 22:09:45 +0100

    Add helpers, provide jsdoc.

--- resources/js/helpers.js
+++ resources/js/helpers.js
@@ -1,4 +1,12 @@
-const linkWithCustomMethod = (method, url) => {
+/**
+ * Create a link that when clicked, submits a form which will send an HTTP call to the given URL.
+ *
+ * @param {String} method The HTTP method name.
+ * @param {Strign} url The URL to make an HTTP call to.
+ *
+ * @return {String} The generated link.
+ */
+export const linkWithCustomMethod = (method, url) => {
     const form = document.createElement('form');
     const methodInput = document.createElement('input');
     const tokenInput = document.createElement('input');
@@ -22,7 +30,21 @@ const linkWithCustomMethod = (method, url) => {
     form.submit();
 };
 
+/**
+ * Retrieve the X-CSRF-TOKEN from the head of the page.
+ *
+ * @return {String} The X-CSRF-TOKEN.
+ */
+export const xsrf_token = () => document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 
-const exposeGlobally = (...fns) => fns.forEach(fn => window[fn.name] = fn);
+/**
+ * Expose a method, as to make it usable throughout the whole application.
+ * When
+ *
+ * @param {Function} fn The function to expose globally
+ * @param {String} name The name under which to expose it.
+ */
+export const exposeGlobally = (fn, name) => window[name] = fn;
 
-exposeGlobally(linkWithCustomMethod);
+exposeGlobally(linkWithCustomMethod, 'linkWithCustomMethod');
+exposeGlobally(xsrf_token, 'xsrf_token');
