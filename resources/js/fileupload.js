c9903831767c62294d11aa2d8f0cbfd6b6c9675b
OneBigOwnage <niekvandenbos@hotmail.com>
2020-01-05 22:06:21 +0100

    Add missing js & css imports, simplify instantiation of filepond.

--- resources/js/fileupload.js
+++ resources/js/fileupload.js
@@ -1,16 +1,21 @@
 import * as FilePond from 'filepond';
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+import { xsrf_token } from './helpers';
+
 import 'filepond/dist/filepond.min.css';
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
+
+FilePond.registerPlugin(FilePondPluginImagePreview);
 
 FilePond.setOptions({
     server: {
         url: '/images',
         headers: {
-            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+            'X-CSRF-TOKEN': xsrf_token()
         }
     },
+});
 
-})
-
-const pond = FilePond.create(
+FilePond.create(
     document.getElementById('pond-container')
 );
