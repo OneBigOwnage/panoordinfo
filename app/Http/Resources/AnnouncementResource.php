520545ab738d54a5c9475607475751a4b23c49cf
OneBigOwnage <niekvandenbos@hotmail.com>
2020-01-05 16:26:54 +0100

    Add resource for announcements.

---
+++ app/Http/Resources/AnnouncementResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AnnouncementResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return array_merge($this->resource->toArray(), [
+            'html' => view('components.announcement')->with('item', $this->resource)->render()
+        ]);
+    }
+}
