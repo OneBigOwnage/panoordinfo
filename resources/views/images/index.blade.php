3833f780f285788480564bcbafefdeb47d9fc31b
OneBigOwnage <niekvandenbos@hotmail.com>
2020-01-05 22:10:21 +0100

    Add file uploads and a list of images to the image page.

--- resources/views/images/index.blade.php
+++ resources/views/images/index.blade.php
@@ -11,15 +11,40 @@
   </ol>
 </nav>
 
-<main>
-  <section>
-    <input type="file" id="pond-container">
+  <section class="image-upload-area">
+      <input type="file" id="pond-container" name="image_file"
+        accept="image/png, image/jpeg, image/gif"
+        {{-- data-instant-upload="false" --}}
+        data-drop-validation="true"
+
+
+        {{-- Styling --}}
+        {{-- data-style-panel-layout="compact circle" --}}
+        data-style-load-indicator-position="center bottom"
+        data-style-progress-indicator-position="right bottom"
+        data-style-button-remove-item-position="left bottom"
+        data-style-button-process-item-position="right bottom"
+        >
   </section>
 
-  <section>
+
+  <section class="image-list">
+
+    @foreach ($images as $image)
+      <div class="img rounded single-image-container">
+        <img src="{{ $image->toShowURL() }}" alt="Image" class="img-thumbnail img-fluid">
+        <div class="overlay bg-dark"></div>
+        <div class="overlay-content text-center justify-content-end">
+          <div class="btn-group mb-1" role="group">
+            <button type="button" class="btn btn-text-light btn-icon rounded-circle shadow-none"><i class="material-icons">zoom_in</i></button>
+            <button type="button" class="btn btn-text-light btn-icon rounded-circle shadow-none"><i class="material-icons">edit</i></button>
+            <a href="javascript:linkWithCustomMethod('DELETE', '{{ route('images.destroy', $image) }}')" title="Verwijderen" class="btn btn-text-light btn-icon rounded-circle shadow-none text-danger"><i class="material-icons">delete</i></a>
+          </div>
+        </div>
+      </div>
+    @endforeach
 
   </section>
-</main>
 
 <script src="{{ mix('/js/fileupload.js') }}"></script>
 
