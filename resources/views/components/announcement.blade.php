7825f6d69473a244ef0cc75f928f63a096604c60
OneBigOwnage <niekvandenbos@hotmail.com>
2020-01-05 16:26:46 +0100

    Add a component for announcements.

---
+++ resources/views/components/announcement.blade.php
@@ -0,0 +1,9 @@
+<div class="panel tinyAnnouncementPanel">
+  <div class="panel-heading">
+    <h3 class="panel-title">
+      <span class="glyphicon glyphicon-bullhorn"></span>
+      {{ $item->title }}
+    </h3>
+  </div>
+  <div class="panel-body">{{ $item->contents }}</div>
+</div>
