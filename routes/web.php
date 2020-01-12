c3b903d80e0a44062a77b8c6071679baf5ac9a02
OneBigOwnage <niekvandenbos@hotmail.com>
2020-01-12 23:30:14 +0100

    Restrict routes with auth, restructure routes file

--- routes/web.php
+++ routes/web.php
@@ -11,30 +11,32 @@
 |
 */
 
-Route::view('/', 'home')->name('home');
 Route::get('/login', 'AuthController@loginPage')->name('login');
 Route::post('/login', 'AuthController@login');
-
-Route::view('/mimity', 'layouts.default');
-Route::prefix('/maintenance')->group(function () {
-    Route::view('/', 'maintenance')->name('maintenance');
-    Route::get('/db-migrate', 'MaintenanceController@DBMigrate')->name('maintenance.db-migrate');
-    Route::get('/db-migrate-fresh', 'MaintenanceController@DBMigrateFresh')->name('maintenance.db-migrate-fresh');
-});
-
-Route::resource('agenda-items', 'AgendaItemController');
-Route::resource('announcements', 'AnnouncementController');
-
-Route::prefix('/images')->group(function () {
-    Route::get('/', 'ImageController@index')->name('images.index');
-    Route::post('/', 'ImageController@upload')->name('images.upload');
-    Route::get('/{image}', 'ImageController@show')->name('images.show');
-    Route::delete('/{image}', 'ImageController@destroy')->name('images.destroy');
-});
 
 Route::prefix('/screen')->group(function () {
     Route::get('/', 'ScreenController@default')->name('screen');
     Route::get('/agenda-items', 'ScreenController@agendaItems')->name('screen.agenda-items');
     Route::get('/announcements', 'ScreenController@announcements')->name('screen.announcements');
     Route::get('/images', 'ScreenController@images')->name('screen.images');
+});
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::view('/', 'home')->name('home');
+
+    Route::resource('agenda-items', 'AgendaItemController');
+    Route::resource('announcements', 'AnnouncementController');
+
+    Route::prefix('/images')->group(function () {
+        Route::get('/', 'ImageController@index')->name('images.index');
+        Route::post('/', 'ImageController@upload')->name('images.upload');
+        Route::get('/{image}', 'ImageController@show')->name('images.show');
+        Route::delete('/{image}', 'ImageController@destroy')->name('images.destroy');
+    });
+
+    Route::prefix('/maintenance')->group(function () {
+        Route::view('/', 'maintenance')->name('maintenance');
+        Route::get('/db-migrate', 'MaintenanceController@DBMigrate')->name('maintenance.db-migrate');
+        Route::get('/db-migrate-fresh', 'MaintenanceController@DBMigrateFresh')->name('maintenance.db-migrate-fresh');
+    });
 });
